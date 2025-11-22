e5d5483844d3614aed99112de90f37af152810d8
JoelDemkollari <joel.demkollari@fermi.mo.it>
2025-11-22 08:34:29 +0100

    Terminato l'handler dei Publisher

--- handlers/handlerPublishers.py
+++ handlers/handlerPublishers.py
@@ -15,6 +15,7 @@ class PublisherHandler(tornado.web.RequestHandler):
     async def get(self, publisher_id = None):
         self.set_header("Content-Type", "application/json")
         
+        #Recupero filtri nel caso in cui ci siano
         pubName = self.get_argument("name", None)
         pubCountry = self.get_argument("country", None)
                 
@@ -25,6 +26,7 @@ class PublisherHandler(tornado.web.RequestHandler):
                 
                 self.set_status(200)
                 self.write(doc)
+
             except Exception as e:
                 self.set_status(400)
                 self.write({"error": str(e)})
@@ -41,3 +43,70 @@ class PublisherHandler(tornado.web.RequestHandler):
             self.write(tornado.escape.json_encode({
                 "publishers": respDict
             }))
+    
+    async def post(self):
+        self.set_header("Content-Type", "application/json")
+        
+        data = tornado.escape.json_decode(self.request.body)
+        data_keys = set(data.keys())
+        
+        req_fields = {"name", "founded_year", "country"}
+
+        mancanti = req_fields - data_keys
+        extra = data_keys - req_fields
+        
+        if mancanti:
+            self.set_status(400)
+            self.write({"error": f"Missing required parameters: {', '.join(mancanti)}"})
+            return
+        
+        if extra:
+            self.set_status(400)
+            self.write({"error": f"Unexpected parameters found: {', '.join(extra)}"})
+            return
+
+        try:
+            await self.pubCollection.insert_one(data)
+            self.set_status(200)
+        except Exception as e:
+            self.set_status(400)
+            self.write({"error": str(e)})
+            
+    async def put(self, publisher_id = None):
+        self.set_header("Content-Type", "application/json")
+        
+        data = tornado.escape.json_decode(self.request.body)
+                
+        req_fields = {"name", "founded_year", "country"}
+
+        extra = set(data.keys()) - req_fields
+        
+        if extra:
+            self.set_status(400)
+            self.write({"error": f"Unexpected parameters found: {', '.join(extra)}"})
+            return
+        
+        if publisher_id:                        
+            update_doc = {param: data[param] for param in data}
+            
+            for param in data:
+                await self.pubCollection.update_one({"_id": ObjectId(publisher_id)}, {"$set": update_doc})
+                self.set_status(200)
+                
+    async def delete(self, publisher_id = None):
+        self.set_header("Content-Type", "application/json")
+        
+        if publisher_id:
+            try:
+                await self.pubCollection.delete_one({"_id": ObjectId(publisher_id)})
+        
+                await self.bookCollection.delete_many({"publisher_id": ObjectId(publisher_id)})
+                
+                self.set_status(200)
+            except Exception as e:
+                self.set_status(400)
+                self.write({"error": str(e)})
+        else:
+            self.set_status(500)
+            self.write({"error": "no publisher id given"})
+            return
